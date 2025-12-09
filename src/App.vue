71af5e9350beb5042ba5aa94304419571e1630d6
Saul Hernandez <shern400@stu.southtexascollege.edu>
2025-12-08 22:47:28 -0600

    Finished Balance

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import Header from './components/Header.vue';
+import Balance from './components/Balance.vue';
+import { computed, ref } from 'vue';
+
+const  transactions = ref([
+  { id: 1, text: 'Paycheck', amount: 700 },
+  { id: 2, text: 'Water Bill', amount: -72.83 },
+  { id: 3, text: 'Electric Bill', amount: -153.89 },
+  { id: 4, text: 'Return Item', amount: 20.00 }, 
+])
+
+const sum = computed(()=>{
+  return transactions.value.reduce((acc, x)=>{
+    return acc + x.amount
+  },0)
+});
+</script>
+
+<template>
+    <Header></Header>
+    <div class="container">
+        <Balance :total="1000"></Balance>
+    </div>
+
+</template>
+
+  
